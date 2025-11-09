46719a378d6aae7b07d121fb76182fbb3562632c
naveen kumar s <getnaveen81@gmail.com>
2025-11-09 11:32:07 +0530

    dummy statement added

--- src/main/java/com/library/model/Book.java
+++ src/main/java/com/library/model/Book.java
@@ -39,8 +39,8 @@ public class Book {
         return title + " by " + author;
     }
 
-    public void dummy2(){
-        System.out.println("dummy");
+    public void dummy(){
+        System.out.println("dummy1");
     }
 
 }
